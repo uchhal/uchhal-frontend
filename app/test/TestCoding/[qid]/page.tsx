5bf45c627b1cbbfd862071cdb6c4aed8acc31036
kapil <kapilchaudhary8280@gmail.com>
2023-08-23 15:35:01 +0530

    coding implimented

---
+++ app/test/TestCoding/[qid]/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Topbar from "@/components/Navbar/Topbar";
+import Workspace from "@/components/Workspace/Workspace";
+import useHasMounted from "@/hooks/useHasMounted";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+
+// const ProblemPage: React.FC<ProblemPageProps> = () => {
+const ProblemPage = () => {
+	const params = useParams();
+	const uid = params.qid;
+	console.log(uid);
+
+	const problem = useGetoneProblem(uid);
+	console.log(problem);
+	
+	
+	// const hasMounted = useHasMounted();
+
+	// if (!hasMounted) return null;
+
+	return (
+		<div>
+			{problem && (<Workspace problem={problem} />)}
+		</div>
+	);
+};
+export default ProblemPage;
+
+function useGetoneProblem(uid: string) {
+	const [problem, setProblem] = useState();
+
+	useEffect(() => {
+		const getProblems = async () => {
+
+		try {
+			const response = await axios.get('http://localhost:8082/coding-bank/'+uid);
+			console.log("response: ", response);
+			console.log(response.data);
+			setProblem(response.data.data);
+			
+		} catch (error: any) {
+			// toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
+			// seterror(error.messsage);
+		}
+		};
+
+		getProblems();
+	}, []);
+	return problem;
+}
--- components/Test/TestQuestions.tsx
+++ components/Test/TestQuestions.tsx
@@ -3,15 +3,18 @@ import Link from "next/link";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import McqCard from "./McqCard";
+import CodingRedirect from "./CodingRedirect"
 function TestQuestions() {
   const params = useParams();
   const uid = params.tid;
   const [mcqProblems, setMcqProblems] = useState([]);
+  const [codingProblems, setCodingProblems] = useState([]);
   useEffect(() => {
     const getProblems = async () => {
       try {
         const response = await axios.get("http://localhost:8082/test/" + uid);
         setMcqProblems(response.data.mcqs);
+        setCodingProblems(response.data.codings);
       } catch (error: any) {
         // toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
         // seterror(error.messsage);
@@ -22,7 +25,7 @@ function TestQuestions() {
   }, [setMcqProblems, uid]);
   return (
     <div className="relative overflow-x-auto mx-2 mt-2">
-      <table className="w-full text-sm text-left text-gray-200 dark:text-gray-100">
+      <table className="w-full text-sm text-left text-gray-400 dark:text-gray-500">
         <thead className="text-xs text-white uppercase bg-gray-600  ">
           <tr>
             <th scope="col" className="px-6 py-3">
@@ -43,6 +46,10 @@ function TestQuestions() {
           {mcqProblems &&
             mcqProblems.map((problem, idx) => (
               <McqCard key={idx} problem={problem} path={uid} />
+            ))}
+          {codingProblems &&
+            codingProblems.map((problem, idx) => (
+              <CodingRedirect path={problem} />
             ))}
         </tbody>
       </table>
