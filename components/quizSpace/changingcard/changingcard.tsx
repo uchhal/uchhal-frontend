432f60839fc2f524c0c357bafd3ea86e385fd698
Rishabh Kumar Gaud <rishabh.kumbh@gmail.com>
2023-09-03 16:27:27 +0530

    popup added

--- components/quizSpace/changingcard/changingcard.tsx
+++ components/quizSpace/changingcard/changingcard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 type Changingcardprops = {
   question: any;
   total: number;
@@ -25,6 +26,15 @@ const changingcard: React.FC<Changingcardprops> = ({
   _term,
   _change,
 }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center">
       {/* <Modal /> */}
@@ -86,12 +96,81 @@ const changingcard: React.FC<Changingcardprops> = ({
             </button>
           )}
           {_term == total && (
-            <button
-              onClick={_result}
+          <div>
+          <button
+              onClick={toggleModal}
               className="py-2 px-7 text-medium flex rounded-lg text-white bg-yellow-600 hover:bg-green-700"
             >
               submit
             </button>
+            {modalVisible && (
+        <div
+          id="popup-modal"
+          className="fixed  z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full max-w-full  "
+        >
+          <div className="relative w-full max-w-md max-h-full right-[-30%] top-[10%]">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 ">
+              <button
+                type="button"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                onClick={closeModal}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+              <div className="p-6 text-center">
+                <svg
+                  className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                  Are you sure you want to submit this test?
+                </h3>
+                <button
+                  type="button"
+                  className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                  onClick={_result}
+                >
+                  Yes, I am sure
+                </button>
+                <button
+                  type="button"
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  onClick={closeModal}
+                >
+                  No, cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+          </div>
           )}
         </div>
       </div>
